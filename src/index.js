64788d74f8331b48d22349f80b1f3308ef724acb
Tanya <tanyamigaluk@gmail.com>
2020-09-30 12:08:29 +0300

    fead: add solution for task typical-array-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ( Array.isArray(array) && array.length) {
+  const minValue = Math.min.apply(null, array);
+  return minValue;
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ( Array.isArray(array) && array.length) {
+  const maxValue = Math.max.apply(null, array);
+  return maxValue;
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
+  if ( Array.isArray(array) && array.length) {
+  var sum = 0;
+  for ( var i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  const avgValue = sum / array.length;
+  return avgValue;
+  } else {
   return 0;
+  }
 }
